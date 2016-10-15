5aef7b67ea0b5116c3a9af13c7aff98fb02d7b74
Venus <vhl8@cornell.edu>
2016-10-14 21:37:41 -0400

    generated images for scene1 and scene2

--- sim_start.py
+++ sim_start.py
@@ -93,7 +93,20 @@ def binarySearch(d, l, u, goal,f):
 
 
 def growth(r, p, B, lam):
-	r_next = math.exp(-lam*1.0*(p-B)/r)
+
+	if r >0.0000001:
+		try:
+			r_next = math.exp(-lam*1.0*(p-B)/r)
+
+		except:
+			print r
+			print p
+			print B
+	else:
+		if p-B<0:
+			r_next = 1.0
+		else:
+			r_next = 0.0
 	return min(1.0, r_next)
 
 
@@ -118,10 +131,10 @@ print scene1.lam
 
 scene1.findOptPrice()
 scene1.find_pLow()
-print scene1.pStar
-print scene1.rStar
-print scene1.rCrit
-print scene1.pLow
+print "scene1 pStar"+str(scene1.pStar)
+print "scene1 rStar" + str(scene1.rStar)
+print "scene1 rCrit" + str(scene1.rCrit)
+print "scene1 pLow" + str(scene1.pLow)
 
 #================================================
 
@@ -129,7 +142,7 @@ for T in [15, 30]:
 
 	gamma = 0.9
 
-	step = 0.0002
+	step = (scene1.pLow-scene1.B)/15
 	xAxis = [i for i in range(0,T)]
 
 	for i in range(0,15):
@@ -146,7 +159,7 @@ for T in [15, 30]:
 		py.ylabel("participation")
 		py.axis([0,T,0,1])
 		py.plot(xAxis, scene1.dict_oneShotDyn[(T,p0)])
-		fig.savefig("Dynamic_p0_"+name+"_T_"+str(T)+".png")
+		fig.savefig("Scene1_Dynamic_p0_"+name+"_T_"+str(T)+".png")
 		py.close(fig)
 
 		fig = py.figure()
@@ -154,7 +167,7 @@ for T in [15, 30]:
 		py.ylabel("revenue")
 		py.axis([0,T,-5.5,3])
 		py.plot(xAxis, scene1.dict_oneShotRev[(T,p0)])
-		fig.savefig("Rev_p0_"+name+"_T_"+str(T)+".png")
+		fig.savefig("Scene1_Rev_p0_"+name+"_T_"+str(T)+".png")
 		py.close(fig)
 
 	x_p0 = [x[1] for x in scene1.dict_profit]
@@ -164,7 +177,74 @@ for T in [15, 30]:
 	fig = py.figure()
 	py.xlabel("initialPrice")
 	py.ylabel("discounted profit")
-	py.axis([-5.002, -4.997,0,7])
+	py.axis([-5.00, -4.997,1,7])
 	py.plot(x_p0,y_profit)
-	fig.savefig("changeDisPro"+str(gamma)+"_T_"+str(T)+".png")
+	fig.savefig("Scene1_changeDisPro"+str(gamma)+"_T_"+str(T)+".png")
 	py.close(fig)
+
+
+
+
+
+
+
+#======================================================
+
+#trait = eps, lambda, B
+
+trait = (0.0001, 1.0, -0.2)
+scene2 = scene(trait)
+
+
+scene2.findOptPrice()
+scene2.find_pLow()
+print "scene2 pLow" + str(scene2.pLow)
+print "scene2 rStar" + str(scene2.rStar)
+print "scene2 rCrit" + str(scene2.rCrit)
+print "scene2 pStar" + str(scene2.pStar)
+#================================================
+
+for T in [15, 30]:
+
+	gamma = 0.9
+
+	step = (scene2.pLow - scene2.B)/15
+	xAxis = [i for i in range(0,T)]
+
+	for i in range(0,15):
+		p0 = scene2.pLow - i*step
+		scene2.oneShotDynamic(T,p0,gamma)
+		#print p0
+		#print scene1.dict_profit[(T,p0)]
+		#print scene1.dict_oneShotDyn[(T, p0)]
+	
+		name = str(p0*10000)[1:6]
+
+		fig = py.figure()
+		py.xlabel("time")
+		py.ylabel("participation")
+		py.axis([0,T,0,1])
+		py.plot(xAxis, scene2.dict_oneShotDyn[(T,p0)])
+		fig.savefig("Scene2_Dynamic_p0_"+name+"_T_"+str(T)+".png")
+		py.close(fig)
+
+		fig = py.figure()
+		py.xlabel("time")
+		py.ylabel("revenue")
+		py.axis([0,T,-0.2,0.2])
+		py.plot(xAxis, scene2.dict_oneShotRev[(T,p0)])
+		fig.savefig("Scene2_Rev_p0_"+name+"_T_"+str(T)+".png")
+		py.close(fig)
+
+	x_p0 = [x[1] for x in scene2.dict_profit]
+	x_p0.sort()
+	y_profit = [scene2.dict_profit[(T,i)] for i in x_p0]
+
+	fig = py.figure()
+	py.xlabel("initialPrice")
+	py.ylabel("discounted profit")
+	py.axis([scene2.B, scene2.pLow,0,1])
+	py.plot(x_p0,y_profit)
+	fig.savefig("Scene2_changeDisPro"+str(gamma)+"_T_"+str(T)+".png")
+	py.close(fig)
+
